499490b78c0b564ab5e1f9fc7e20a5ae3bf8d5ab
Prateek Sunal <prtksunal@gmail.com>
2022-10-13 19:43:03 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_painter/image_painter.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() => runApp(const ExampleApp());
+
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Painter Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const ImagePainterExample(),
+    );
+  }
+}
+
+class ImagePainterExample extends StatefulWidget {
+  const ImagePainterExample({super.key});
+
+  @override
+  State<ImagePainterExample> createState() => _ImagePainterExampleState();
+}
+
+class _ImagePainterExampleState extends State<ImagePainterExample> {
+  final _imageKey = GlobalKey<ImagePainterState>();
+  final _key = GlobalKey<ScaffoldState>();
+
+  void saveImage() async {
+    final messenger = ScaffoldMessenger.of(context);
+    final image = await _imageKey.currentState?.exportImage();
+    final directory = (await getApplicationDocumentsDirectory()).path;
+    await Directory('$directory/sample').create(recursive: true);
+    final fullPath =
+        '$directory/sample/${DateTime.now().millisecondsSinceEpoch}.png';
+    final imgFile = File(fullPath);
+    imgFile.writeAsBytesSync(image!);
+    messenger.showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.grey[700],
+        padding: const EdgeInsets.only(left: 10),
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Image Exported successfully.",
+                style: TextStyle(color: Colors.white)),
+            TextButton(
+              onPressed: () => OpenFile.open(fullPath),
+              child: Text(
+                "Open",
+                style: TextStyle(
+                  color: Colors.blue[200],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showSavePopup() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Save Image"),
+        content: const Text("You have to watch a video ad to save the image."),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              saveImage();
+            },
+            child: const Text("Watch Ad and Save"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        key: _key,
+        body: ImagePainter(
+          key: _imageKey,
+          scalable: true,
+          width: 800,
+          height: 800,
+          extraWidget: IconButton(
+            onPressed: showSavePopup,
+            icon: const Icon(CupertinoIcons.arrow_down_to_line),
+          ),
+          initialStrokeWidth: 2,
+          initialColor: Colors.green,
+          initialPaintMode: PaintMode.line,
+          controlsAtTop: false,
+        ),
+      ),
+    );
+  }
+}
